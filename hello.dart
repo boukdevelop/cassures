aeab9f3004de73b253afc72719390780ab384b77
FRANCK <boukalafranck0@gmail.com>
2026-09-18 07:37:51 +0200

    Derniere modif

--- hello.dart
+++ hello.dart
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-void main(){
-	print("Hello le monde en dart!");
-	return 0;
-=======
-enum Top{
-	MAUVAIS,
-	BON,
-	NEUTRE
->>>>>>> top2
+void main() {
+  print("Hello le monde en dart!");
 }
